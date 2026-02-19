134b593049c1698220af3bc7c28a8f202475db8c
Darien Bernal <darien.bernal@gmail.com>
2026-02-19 17:53:44 -0500

    Created lookup tablee for GPA

--- grade-classifier/script.js
+++ grade-classifier/script.js
@@ -25,3 +25,20 @@ function getGrade(score) {
     } else 
         return "F";
 }
+
+// Write a second function getGPA that takes the letter grade and returns the GPA value (A → 4.0, B → 3.0, etc.)
+// Chain them together so getGPA(getGrade(85)) returns 3.0
+
+function getGPA(letterGrade) {
+    const grades = {
+        A: 4.0,
+        B: 3.0,
+        C: 2.0,
+        D: 1.0,
+        F: 0
+    }
+
+    return grades[letterGrade];
+}
+
+console.log(getGPA(getGrade(63)));
